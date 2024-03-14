7216397f09bab09f1dfb22b64de3eaa8fc4be630
Mohamad Addasi <maddasi04@gmail.com>
2024-03-14 04:03:52 -0400

    Creating a personal portfolio wesbite using angular, css and bootstrap.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  //{ path: 'about', component: AboutComponent },
+  //{path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
