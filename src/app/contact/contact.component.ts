afd196ef9d229f92e58e2d9b2c098e0904f9d14d
Mohamad Addasi <maddasi04@gmail.com>
2024-05-01 02:13:24 -0400

    Added a feature where the submit button for the contact form changes colors if all form fields are filled.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  ngOnInit(): void {
+    console.log("ngOnInitIsCalled");
+    const submitBtn = document.getElementById('submitBtn') as HTMLButtonElement;
+    const formInputs = document.querySelectorAll(
+      '.form-group input, .form-group textarea'
+    );
+    console.log("formInputs", formInputs);
+    console.log("submitBtn", submitBtn);
 
+    function checkFormInputs() {
+      console.log("checkFormInputs is called");
+      let allFilled = true;
+      formInputs.forEach((input: HTMLInputElement) => {
+        if (input.value.trim() === '') {
+          allFilled = false;
+        }
+      });
+
+
+      if (allFilled) {
+        submitBtn.classList.add('form-filled');
+      } else {
+        submitBtn.classList.remove('form-filled');
+      }
+    }
+
+    formInputs.forEach((input: HTMLInputElement) => {
+      input.addEventListener('input', checkFormInputs);
+    });
+  }
 }
